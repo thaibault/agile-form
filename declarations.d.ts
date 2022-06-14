660f121da3012a1bc2070de08a9b2019b588e6fc
torben <t.sickert[~at~]gmail.com>
2022-06-14 18:27:35 +0200

    Automatic update to new version.

--- declarations.d.ts
+++ declarations.d.ts
@@ -2,6 +2,8 @@
 /** @module declarations */
 /// <reference path="node_modules/weboptimizer/declarations" />
 declare module 'react-useanimations';
+declare module 'react-useanimations/lib/lock';
+declare module 'react-useanimations/lib/plusToX';
 // region vim modline
 // vim: set tabstop=4 shiftwidth=4 expandtab:
 // vim: foldmethod=marker foldmarker=region,endregion:
