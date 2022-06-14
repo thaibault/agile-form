69ceee6163c6a7fb09a5a38a0c35e674973bc966
torben <t.sickert[~at~]gmail.com>
2022-06-14 18:23:16 +0200

    Automatic update to new version.

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,6 +1,7 @@
 // -*- coding: utf-8 -*-
 /** @module declarations */
 /// <reference path="node_modules/weboptimizer/declarations" />
+declare module 'react-useanimations';
 // region vim modline
 // vim: set tabstop=4 shiftwidth=4 expandtab:
 // vim: foldmethod=marker foldmarker=region,endregion:
