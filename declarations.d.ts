625debfb572ed5e56b2af8d0f450d05dde60ca93
thaibault <info@torben.website>
2024-07-18 17:32:33 +0200

    Automatic update to new version.

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,6 +1,5 @@
 // -*- coding: utf-8 -*-
 /** @module declarations */
-import 'node_modules/weboptimizer/declarations';
-declare module 'react-useanimations';
-declare module 'react-useanimations/lib/lock';
-declare module 'react-useanimations/lib/plusToX';
+declare module 'react-useanimations'
+declare module 'react-useanimations/lib/lock'
+declare module 'react-useanimations/lib/plusToX'
